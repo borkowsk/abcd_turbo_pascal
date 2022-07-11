8bc222e0c69c72ec3c024a6d6d7a5442d994818b
Wojciech T. Borkowski <wborkowsk@gmail.com>
2022-07-11 23:39:53 +0200

    Pierwsze podejście do tłumaczenia na C++

---
+++ pixels.cxx
@@ -0,0 +1,29 @@
+#include "ptoc.h"
+
+
+#include "graph.h"
+/*#include "graph2.h"*/
+/*#include "crt.h"*/
+
+
+integer x,y;
+word c;
+real r;
+
+int main(int argc, const char* argv[])
+{
+
+pio_initialize(argc, argv);
+graph2init("",nil);
+Randomize();
+
+do {
+x=1+Random(mx);
+y=1+Random(my);
+r=abs(x-y);
+c=round(maxcolor*sqrt(r));
+putpixel(x,y,c);
+} while (!keypressed());
+
+return EXIT_SUCCESS;
+}
