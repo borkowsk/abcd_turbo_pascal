65141ad4099d3549ba6d4b3a5fda41e18639b8c0
Wojciech T. Borkowski <wborkowsk@gmail.com>
2022-07-11 23:50:57 +0200

    Tried_ptoc

--- pixels.pas
+++ pixels.pas
@@ -1,11 +1,15 @@
 program pixels;
 uses graph,graph2,crt;
+
 var x,y:integer;
     c:word;
     r:real;
+
 BEGIN
+
 graph2init('',nil);
 randomize;
+
 repeat
 x:=1+random(mx);
 y:=1+random(my);
@@ -13,4 +17,5 @@ r:=abs(x-y);
 c:=round(maxcolor*sqrt(r));
 putpixel(x,y,c);
 until keypressed;
+
 END.
